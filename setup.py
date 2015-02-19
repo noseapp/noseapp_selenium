c76319958e91cb62e2182cc48471367a83266d4a
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-02-19 22:26:34 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ from setuptools import find_packages
 import noseapp_selenium_ex
 
 
+with open('requirements.txt') as fp:
+    requirements = [req.strip() for req in fp.readlines() if not req.startswith('--')]
+
+
 if __name__ == '__main__':
     setup(
         name='noseapp_selenium_ex',
+        url='https://github.com/trifonovmixail/noseapp_selenium',
         version=noseapp_selenium_ex.__version__,
         packages=find_packages(),
         author='Mikhail Trifonov',
         author_email='mikhail.trifonov@corp.mail.ru',
         description='selenium extension for noseapp lib',
-        install_requires=[
-            'selenium==2.44.0',
-        ],
+        include_package_data=True,
+        zip_safe=False,
+        platforms='any',
+        install_requires=requirements,
     )
